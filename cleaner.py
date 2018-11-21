e00786575f25e16f37eafb7248752f6410af5785
annemwm <amwmeisner@gmail.com>
2018-11-21 15:41:59 -0500

    leading numeric errors on cleaning pass

--- cleaner.py
+++ cleaner.py
@@ -1,6 +1,5 @@
 
-
-
+import re
 # with open("SentenceAligned.v2.txt", "r") as corpus, open("English_Parsed.txt", "w") as engl, open("Inuktitut_Parsed.txt", "w") as inuk, open("exclude.txt", "w") as ex:
 #     line_index = 0
 #     for line in corpus:
@@ -22,5 +21,8 @@ with open("SentenceAligned.v2.txt", "r") as corpus, open("English_Parsed.txt", "
         else:
             if p[1].strip() == "":
                 continue
+            if ("moZos3=1u vtm0Jtu#5" in p[0]):
+                continue
+            p[0]= re.sub(r"[0-9]{8}", "", p[0])
             engl.write(p[1]+"\r\n")
             inuk.write(p[0]+"\r\n")
