34f7cdced1a85f848b76dd28db3a98132476963c
annemwm <amwmeisner@gmail.com>
2018-12-09 11:55:46 -0500

    include morphemes in test data

--- gen_set.py
+++ gen_set.py
@@ -21,3 +21,15 @@ with open("Inuktitut_Parsed.txt", "r") as inuk, open("English_Parsed.txt", "r") 
             if (ct >= 112000):
                 continue
         ct += 1
+
+with open("Underlying_Morpheme.txt", "r") as inuk, open("morphtune.in", "w") as itune, open("morphtest.in", "w") as itest:
+    ct = 1
+    for iline in inuk:
+        if ((ct >= 100000) & (ct < 106000)):
+            itune.write(iline)
+        elif ((ct >=106000) & (ct < 112000)):
+            itest.write(iline)
+        else:
+            if (ct >= 112000):
+                continue
+        ct += 1
